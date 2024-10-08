ef68ce503b484b3ce6167dbe519f074870bf6a1b
phattran <phattran052004@gmail.com>
2024-10-08 19:08:54 +0700

    config cookie and cor

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.use(
   cors({
     origin: "https://silly-pika-5bfefb.netlify.app",
     credentials: true,
-    methods: "GET,POST,PUT,DELETE",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
   })
 );
