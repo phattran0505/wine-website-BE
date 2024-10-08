08ff7bb62c67a5dcdd30e05d2c5806ef15a12115
phattran <phattran052004@gmail.com>
2024-10-08 19:38:40 +0700

    update config cors

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const connectDB = async () => {
 // middlewares
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
